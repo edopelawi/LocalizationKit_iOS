286ae721170aad454271a5432a80592dade78ade
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2017-04-03 17:15:52 +0700

    Add file from Example/Pods

---
+++ Example/Pods/Target Support Files/Pods-LocalizationKit_Example/Pods-LocalizationKit_Example-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_LocalizationKit_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_LocalizationKit_ExampleVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-LocalizationKit_Tests/Pods-LocalizationKit_Tests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_LocalizationKit_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_LocalizationKit_TestsVersionString[];
+
